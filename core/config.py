be7c9ab5ccd54c471df8c052e601d38169aab42b
Sina Alavi <atom-jogger-freely@duck.com>
2026-09-11 16:17:40 +0330

    Update config.py

--- core/config.py
+++ core/config.py
@@ -23,8 +23,8 @@ INFO_CHANNEL_ID = os.getenv("INFO_CHANNEL_ID", "5524168471")
 ADMIN_IDS = [234591600]
 
 # API Settings
-ITUNES_BASE_URL = os.getenv("ITUNES_BASE_URL", "https://3rah.ir/mm/api")
-API_BASE_URL = os.getenv("API_BASE_URL", "https://3rah.ir/users/index.php")
+ITUNES_BASE_URL = os.getenv("ITUNES_BASE_URL", "https://musicman.gt.tc/api")
+API_BASE_URL = os.getenv("API_BASE_URL", "https://musicman.gt.tc/users/index.php")
 API_TOKEN = os.getenv("API_TOKEN", "change_me_to_a_secure_token")
 
 # Spotify Credentials
